2933ab32e79992f2f7a0e07bcce316c79d2a6448
Silas <flacezockt@gmail.com>
2024-11-07 23:03:42 +0100

    log

--- src/api_key.js
+++ src/api_key.js
@@ -5,20 +5,24 @@ let apiKey = null;
 let updateInterval = 1000 * 60 * 60 * 24; // 24 hours
 
 async function updateAPIKey() {
-    console.log("Updating Keygen API key");
-    const token = await keygen.createToken(
-        process.env.KEYGEN_ACCOUNT_EMAIL,
-        process.env.KEYGEN_ACCOUNT_PASSWORD
-    )
+  console.log("Updating Keygen API key");
+  const token = await keygen.createToken(
+    process.env.KEYGEN_ACCOUNT_EMAIL,
+    process.env.KEYGEN_ACCOUNT_PASSWORD
+  );
 
-    apiKey = token.attributes.token;
-    lastUpdate = Date.now();
+  apiKey = token.attributes.token;
+  lastUpdate = Date.now();
 }
 
 export default async function getAPIKey() {
-    if (apiKey === null || lastUpdate === null || Date.now() - lastUpdate > updateInterval) {
-        await updateAPIKey();
-    }
-
-    return apiKey;
+  if (
+    apiKey === null ||
+    lastUpdate === null ||
+    Date.now() - lastUpdate > updateInterval
+  ) {
+    await updateAPIKey();
+  }
+  console.log(apiKey);
+  return apiKey;
 }
--- src/keygen.js
+++ src/keygen.js
@@ -1,8 +1,8 @@
-import Keygen from "@themaximalist/keygen.js"
+import Keygen from "@themaximalist/keygen.js";
 
 const keygen = new Keygen({
-    account_id: process.env.KEYGEN_ACCOUNT_ID,
-    base_url: process.env.KEYGEN_BASE_URL,
+  account_id: process.env.KEYGEN_ACCOUNT_ID,
+  base_url: process.env.KEYGEN_BASE_URL,
 });
 
 export default keygen;
