7d1b9a7fc9f203ae880af58c5f3f720348752e1d
Silas <flacezockt@gmail.com>
2024-11-07 16:02:39 +0100

    added docker support

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# Verwende ein Node.js-Image
+FROM node:18
+
+# Installiere PM2 global
+RUN npm install -g pm2
+
+# Arbeitsverzeichnis im Container setzen
+WORKDIR /app
+
+# Kopiere package.json und package-lock.json
+COPY package*.json ./
+
+# Installiere Abhängigkeiten
+RUN npm install
+
+# Kopiere den restlichen Code ins Arbeitsverzeichnis
+COPY . .
+
+# Baue Tailwind CSS für die Produktion
+RUN npm run build:css
+
+# Exponiere den Port, den die App verwendet
+EXPOSE 3000
+
+# Starte den Server mit PM2 im Cluster-Modus
+CMD ["pm2-runtime", "src/index.js", "--name", "keygen-admin", "--watch"]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{html,js}",
-        "./views/**/*.{html,ejs}"
-    ],
-    theme: {
-        extend: {
-            screens: {
-                '3xl': '1700px',
-            },
-        },
+  content: ["./src/**/*.{html,js}", "./views/**/*.{html,ejs}"],
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "1700px",
+      },
     },
-    plugins: [
-        require("@tailwindcss/forms"),
-        require("@tailwindcss/typography")
-    ]
-}
-
+  },
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
